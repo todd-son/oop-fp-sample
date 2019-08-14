cfb54eb45568e3443e4d5189a0011f58efa648a0
todd <devsun98@gmail.com>
2019-08-14 14:28:03 +0900

    Merge branch 'feature/ch01' into feature/ch02
    # Conflicts:
    #	src/main/scala/job/ch01/Job.scala

--- src/main/scala/cafe/ch02/Cafe.scala
+++ src/main/scala/cafe/ch02/Cafe.scala
@@ -2,13 +2,13 @@ package cafe.ch02
 
 import scala.util.{Failure, Success, Try}
 
-case class Customer(cashier: Cashier, coffee: Option[Coffee] = None) {
-  def order(coffeeName: String, quantity: Int): Customer = {
-    val coffee = cashier.order(coffeeName, quantity)
+case class Todd(eileen: Eileen, coffee: Option[Americano] = None) {
+  def order(coffeeName: String, quantity: Int): Todd = {
+    val coffee = eileen.order(coffeeName, quantity)
     this.copy(coffee = Some(coffee))
   }
 
-  def drinkCoffee(quantity: Int): Try[Customer] = {
+  def drinkCoffee(quantity: Int): Try[Todd] = {
     coffee match {
       case None => Failure(new IllegalStateException("Don't have a cup of coffee."))
       case Some(c) =>
@@ -20,16 +20,16 @@ case class Customer(cashier: Cashier, coffee: Option[Coffee] = None) {
   }
 }
 
-case class Cashier(barista: Barista) {
-  def order(coffeeName: String, quantity: Int): Coffee = barista.makeCoffee(coffeeName, quantity)
+case class Eileen(shell: Shell) {
+  def order(coffeeName: String, quantity: Int): Americano = shell.makeCoffee(coffeeName, quantity)
 }
 
-case class Barista() {
-  def makeCoffee(coffeeName: String, quantity: Int): Coffee = Coffee(coffeeName, quantity)
+case class Shell() {
+  def makeCoffee(coffeeName: String, quantity: Int): Americano = Americano(quantity)
 }
 
-case class Coffee(name: String, quantity: Int) {
-  def drunken(quantity: Int): Try[Coffee] =
+case class Americano(quantity: Int) {
+  def drunken(quantity: Int): Try[Americano] =
     if (this.quantity < quantity)
       Failure(new IllegalStateException("Not enough coffee."))
     else
